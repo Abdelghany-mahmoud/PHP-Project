30154b6cf0e8b5b18394f9fe5005030c474b7f43
Abdelghany-mahmoud <abdelghany1699@gmail.com>
2024-08-20 15:20:13 +0300

    updated some functionality

--- index.php
+++ index.php
@@ -9,39 +9,35 @@
   $query2 = 'select * from orders';
   $query3 = 'select * from order_items';
   $query4 = 'select * from products';
-  var_dump($ob->selectAll($query));
+  // var_dump($ob->selectAll($query));
   var_dump($ob->selectAll($query2));
   var_dump($ob->selectAll($query3));
-  var_dump($ob->selectAll($query4));
+  // var_dump($ob->selectAll($query4));
 
-  $Uid = [1];
+  $Uid = 1;
 
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $queryi = 'insert into orders (user_id) values(?)';
-    $ob->DML($queryi, $Uid);
+    $ob->DML($queryi, [$Uid]);
     $LOI_query = 'SELECT id FROM orders ORDER BY ID DESC LIMIT 1'; //LOI 'last order id'
     $LOI = $ob->selectAll($LOI_query)[0]['id'];
 
     foreach ($_POST as $k => $v) {
       $Item_Price_query = 'select price from products where id =?';
-      $K = [$k];
-      $Item_Price = $ob->select($Item_Price_query, $K)[0]['price'];
+      $Item_Price = $ob->select($Item_Price_query, [$k])[0]['price'];
       $priceTotal = $Item_Price * $v;
       $queryi2 = 'INSERT INTO order_items values(?,?,?,?)';
-      $arr = [$LOI, $k, $v, $priceTotal];
-      $ob->DML($queryi2, $arr);
+      $ob->DML($queryi2, [$LOI, $k, $v, $priceTotal]);
     }
 
     var_dump($ob->selectAll($query2));
     var_dump($ob->selectAll($query3));
 
     $calc_price_query = 'SELECT sum(price) as total_price from order_items where order_id=?';
-    $LOI2 = [$LOI];
-    $Total_Order_Price = $ob->select($calc_price_query, $LOI2)[0]['total_price'];
+    $Total_Order_Price = $ob->select($calc_price_query, [$LOI])[0]['total_price'];
     $update_price_query = 'update orders set total_price =? where id=?';
-    $arr = [$Total_Order_Price, $LOI];
-    $ob->DML($update_price_query, $arr);
+    $ob->DML($update_price_query, [$Total_Order_Price, $LOI]);
 
     var_dump($ob->selectAll($query2));
   }
--- product.php
+++ product.php
@@ -15,20 +15,6 @@ class Product
 
   public function add_user()
   {
-    if (isset($_FILES['image'])) {
-      $upload_dir = '../uploads/users/';
-      $product_image = $upload_dir . basename($_FILES['profile_pic']['name']);
-      move_uploaded_file($_FILES['image']['tmp_name'], $product_image);
-    } else {
-      $product_image = '';
-    }
 
-    $query = "insert into users(name, email, password,room_no,ext,profile_pic,is_admin) values (?,?,?,?,?,?,?)";
-
-    $stmt = $this->conn->prepare($query);
-
-    $stmt->execute([$_POST['name'], $_POST['email'], $hashed_password, $_POST['room_no'], $_POST['ext'], $upload_dir . $_FILES['profile_pic']['name'], $_POST['is_admin']]);
-
-    echo "user added successfully";
   }
 }
